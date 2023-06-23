92ee5000d6d63701d4277674f83819d8845a16d7
Marek <marekdrag@gmail.com>
2023-06-23 09:55:18 +0200

    Enhanced Login component with error handling and input validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,17 +6,29 @@ import './Login.css';
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleSubmit = async event => {
-    event.preventDefault();
-
-    const user = {
-      username: username,
-      password: password,
-    };
-
-    const response = await axios.post('http://localhost:8000/api-user/login', user);
-    console.log(response.data);
+      event.preventDefault();
+  
+      // Validation
+      if (!username || !password) {
+          setError("Please fill in all fields correctly.");
+          return;
+      }
+  
+      const user = {
+          username: username,
+          password: password,
+      };
+  
+      try {
+          const response = await axios.post('http://localhost:8000/api-user/login', user);
+          console.log(response.data);
+      } catch (err) {
+          // Assuming the API returns a message in case of an error
+          setError(err.response.data.message);
+      }
   };
 
   return (
@@ -24,6 +36,7 @@ function Login() {
       <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
       <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
       <button type="submit">Login</button>
+      {error && <div className="error-message">{error}</div>}
     </form>
   );
 }
