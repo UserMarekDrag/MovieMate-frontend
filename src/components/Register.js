731652c103a1eb6c650f1b0821bbf62136586ed6
Marek <marekdrag@gmail.com>
2023-06-23 10:05:48 +0200

    Added email and confirm password fields, terms acceptance, error handling
    This update includes several enhancements to the Register component. Added are email and confirmPassword fields to the registration form. Users now also have to accept terms and conditions to be able to register, which is tracked by the acceptTerms state. Lastly, error handling has been improved: an error message is displayed if fields are not filled out correctly or if the API request fails.

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,25 +5,49 @@ import './Register.css';
 
 function Register() {
   const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [acceptTerms, setAcceptTerms] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSubmit = async event => {
-    event.preventDefault();
-
-    const user = {
-      username: username,
-      password: password,
-    };
-
-    const response = await axios.post('http://localhost:8000/api-user/register', user);
-    console.log(response.data);
+      event.preventDefault();
+  
+      // Validation
+      if (!username || !password || !email || password !== confirmPassword || !acceptTerms) {
+          setError("Please fill in all fields correctly and accept the terms and conditions.");
+          return;
+      }
+  
+      const user = {
+          username: username,
+          password: password,
+          email: email
+      };
+  
+      try {
+          const response = await axios.post('http://localhost:8000/api-user/register', user);
+          console.log(response.data);
+      } catch (err) {
+          // Assuming the API returns a message in case of an error
+          setError(err.response.data.message);
+      }
   };
+  
 
   return (
     <form className="register-form" onSubmit={handleSubmit}>
-      <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
-      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+      <input type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" required/>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required/>
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" required/>
+      <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirm Password" required/>
+      <div className="checkbox-container">
+        <input type="checkbox" checked={acceptTerms} onChange={e => setAcceptTerms(e.target.checked)} />
+        <span>I accept the terms and conditions</span>
+      </div>
       <button type="submit">Register</button>
+      {error && <div className="error-message">{error}</div>}
     </form>
   );
 }
