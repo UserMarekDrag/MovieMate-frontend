43c721b6d2022d284cce2f40184b4ef28ce2f3ac
Marek <marekdrag@gmail.com>
2023-06-25 17:03:47 +0200

    Added transition to Hamburger Menu as well as supporting libraries
    In this commit, the Navbar component was updated to display its menu items in a Hamburger Menu on smaller screens for better mobile user experience. To facilitate this, necessary libraries were added to support the functionality of the Hamburger Menu. This enhances the application's responsiveness and usability on mobile devices.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+
 
 import './Navbar.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    document.body.classList.toggle('overflow-hidden');
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+    document.body.classList.remove('overflow-hidden');
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar-brand">MovieMate</Link>
-      <div className="navbar-links">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/profile">Profile</Link>
+      <div className="navbar-section" />
+      <Link to="/" className="navbar-brand" onClick={closeMenu}>MovieMate</Link>
+      <div className="navbar-section">
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+        </div>
+        <div className={`navbar-links ${isOpen ? "show-nav" : ""}`}>
+          <Link to="/" onClick={closeMenu}>Search</Link>
+          <Link to="/login" onClick={closeMenu}>Login</Link>
+          <Link to="/register" onClick={closeMenu}>Register</Link>
+          <Link to="/profile" onClick={closeMenu}>Profile</Link>
+        </div>
       </div>
     </nav>
   );
