0b16d13cd7c326b449eaaea0b1e99dacb9552276
Marek <marekdrag@gmail.com>
2023-06-30 18:18:26 +0200

    Refactored input fields to use InputField in ResetPassword component
    - Refactored the inputs in the ResetPassword component to use the reusable InputField component.
    - Adjusted the relevant logic and error handling to work with the new InputField structure.
    - Adjusted CSS styles to unify.
    - Enhanced the readability and maintainability of the code by leveraging the InputField component for input management.

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,49 +1,58 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import InputField from './InputField';
 
 import './ResetPassword.css';
 
 function ResetPassword() {
   const [email, setEmail] = useState("");
-  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState({});
 
   const handleSubmit = async event => {
-      event.preventDefault();
-      
-      // Clear error state
-      setError(null);
+    event.preventDefault();
 
-      // Validation
-      if (!email) {
-          setError("Please fill in email field.");
-          return;
-      }
-  
-      const user = {
-          email: email
-      };
-  
-      try {
+    const validate = () => {
+      let newErrors = {};
+
+      // email validation
+      if (!email) newErrors.email = 'Email is required.';
+      else if (!/\S+@\S+\.\S+/.test(email)) errors.email = 'Email is not valid.';
+      
+      setErrors(newErrors);
+
+      // if newErrors is empty return true, else false
+      return Object.keys(newErrors).length === 0 ? null : newErrors;
+    };
+
+    const errors = validate()
+
+    if (!errors) {
+        const user = {
+          email
+        };
+
+        try {
           const response = await axios.post('http://localhost:8000/api-user/reset', user);
           console.log(response.data);
-      } catch (err) {
+        } catch (err) {
           // Assuming the API returns a message in case of an error
-          setError(err.response.data.message);
-      }
+          setErrors({...errors, api: err.response.data.message});
+        }
+    }   
   };
   
 
   return (
     <form className="password-form" onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required/>
+      <InputField type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" error={errors.email} />
       <button type="submit">Reset password</button>
-      {error && <div className="error-message">{error}</div>}
+      {errors.api && <div className="error-message">{errors.api}</div>}
       <div class="password-links">
-        Requires a verified email address.
+        <span>Requires a verified email address.</span>
       </div>
       <div class="password-links">
-        Already have an account?
+        <span>Already have an account?</span>
         <Link to="/login">Sign in</Link>
       </div>
     </form>
