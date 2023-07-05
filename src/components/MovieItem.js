fdab79f0d7b7e33ed4a803799489bea1daa938a0
Marek <marekdrag@gmail.com>
2023-07-05 13:26:14 +0200

    Adjusted data mapping and CSS in MovieItem for list layout

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -4,14 +4,16 @@ import './MovieItem.css';
 const MovieItem = ({ movie }) => {
   return (
     <div className="movie-card">
-      <img src={movie.posterUrl} alt={movie.title} className="movie-poster" />
-      <h2 className="movie-title">{movie.title}</h2>
-      <p className="movie-description">{movie.description}</p>
-      <div className="movie-details">
-        <span>{movie.cinemaName}</span>
-        <span>{movie.showTime}</span>
+      <img src={movie.movie.image_url} alt={movie.movie.title} className="movie-poster" />
+      <div className="movie-info">
+        <h2 className="movie-title">{movie.movie.title}</h2>
+        <p className="movie-description">{movie.movie.description}</p>
+        <div className="movie-details">
+          <span>{movie.cinema.name}</span>
+          <span>{movie.time}</span>
+        </div>
+        <a href={movie.booking_link} target="_blank" rel="noopener noreferrer" className="movie-link">More info</a>
       </div>
-      <a href={movie.link} target="_blank" rel="noopener noreferrer" className="movie-link">More info</a>
     </div>
   );
 };
