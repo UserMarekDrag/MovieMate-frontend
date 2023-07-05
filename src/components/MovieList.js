6e78313b8711a9edd37c37946399feed3fd39869
Marek <marekdrag@gmail.com>
2023-07-05 13:24:12 +0200

    Handle form parameters and adjust API endpoint URL in MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 
 import MovieItem from './MovieItem';
 import './MovieList.css';
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
     const fetchMovies = async () => {
-      const result = await axios('http://localhost:8000/api-movie/cinema_in_city');
+      const searchParams = new URLSearchParams(location.search);
+      const city = searchParams.get('city');
+      const date = searchParams.get('date');
+
+      const result = await axios(`http://127.0.0.1:8000/api-movie/cinemas_in_city/?cinema__city=${city}&date=${date}`);
       setMovies(result.data);
     };
 
     fetchMovies();
-  }, []);
+  }, [location]);
 
   return (
     <div className="movie-list">
-      {movies.map(movie => <MovieItem key={movie.id} movie={movie} />)}
+      {movies.map(movie => <MovieItem key={movie.booking_link} movie={movie} />)}
     </div>
   );
 }
