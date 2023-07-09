92110992f9741fd0ffb6ef7a1e75b1b642230307
Marek <marekdrag@gmail.com>
2023-07-09 19:24:49 +0200

    Added interactive Showtimes button to MovieItem component

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,18 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './MovieItem.css';
 
 function MovieItem({ movieSessions }) {
+  const [showTimesVisible, setShowTimesVisible] = useState(false);
   const firstMovieSession = movieSessions[0];
+
+  const handleShowtimesToggle = () => {
+    setShowTimesVisible(!showTimesVisible);
+  };
 
   return (
     <div className="movie-card">
       <img className="movie-poster" src={firstMovieSession.movie.image_url} alt={`${firstMovieSession.movie.title} poster`} />
       <div className="movie-info">
         <h3>{firstMovieSession.movie.title}</h3>
-        {movieSessions.map((session, index) => (
-          <a className="movie-link" href={session.booking_link} key={index}>Showtime: {session.time}</a>
-        ))}
+        <button className="showtimes-toggle" onClick={handleShowtimesToggle}>
+          Showtimes
+        </button>
+        <div className={`showtimes-container ${showTimesVisible ? 'visible' : ''}`}>
+          {movieSessions.map((session, index) => (
+            <a className="movie-link" href={session.booking_link} key={index}>{session.time}</a>
+          ))}
+        </div>
       </div>
     </div>
   );
