7e2f2896569559badb0c70cc1fbc3601d363f3e5
Marek <marekdrag@gmail.com>
2023-07-21 19:58:47 +0200

    Implemented Token Auth in Profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,42 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
 import './Profile.css';
 
-function Profile() {
+const Profile = () => {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
-  const [newUsername, setNewUsername] = useState("");
 
   useEffect(() => {
     const fetchUser = async () => {
-      const result = await axios('http://localhost:8000/api-user/user');
-      setUsername(result.data.username);
+      try {
+        const result = await axios.get('http://localhost:8000/api-user/user/', {
+          headers: { 'Authorization': 'Token ' + localStorage.getItem('token') }
+        });
+        setUsername(result.data.user.username);
+      } catch (error) {
+        console.error(error);
+        navigate('/login');
+      }
     };
 
     fetchUser();
-  }, []);
-
-  const handleSubmit = async event => {
-    event.preventDefault();
-
-    const user = {
-      username: newUsername,
-    };
-
-    const response = await axios.put('http://localhost:8000/api-user/user', user);
-    setUsername(response.data.username);
-    setNewUsername("");
-  };
+  }, [navigate]);
 
   return (
     <div className="profile">
       <h2>Profile</h2>
       <p>User name: {username}</p>
-
-      <form className="profile-form" onSubmit={handleSubmit}>
-        <input type="text" value={newUsername} onChange={e => setNewUsername(e.target.value)} placeholder="New username" />
-        <button type="submit">Update</button>
-      </form>
+      <div className="login-links">
+        <span>Change your password.</span>
+        <Link to="/password/change">Change password</Link>
+      </div>
     </div>
   );
 }
