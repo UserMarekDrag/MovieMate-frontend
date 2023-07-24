d524439ef2fac39781d54e24ec16338e94f0b4d3
Marek <marekdrag@gmail.com>
2023-07-24 18:33:12 +0200

    Implement react-toastify in Logout component

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Logout = () => {
   const navigate = useNavigate();
@@ -11,6 +12,7 @@ const Logout = () => {
         headers: { 'Authorization': 'Token ' + localStorage.getItem('token') }
       });
       localStorage.removeItem('token');
+      toast.success('Successfully logout!');
       navigate('/login');
     } catch (error) {
       console.error(error);
