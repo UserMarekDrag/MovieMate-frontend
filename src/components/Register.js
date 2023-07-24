c17392a9174c9b4a4c4bb4cc146895d6bd0fe302
Marek <marekdrag@gmail.com>
2023-07-24 18:31:24 +0200

    Implement react-toastify in Register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { validateRegister } from './Validation';
 import InputField from './InputField';
+import { toast } from 'react-toastify';
 
 import './Register.css';
 
 function Register() {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -16,28 +18,38 @@ function Register() {
 
   const handleSubmit = async event => {
     event.preventDefault();
-
-    const errors = validateRegister(username, email, password, confirmPassword, acceptTerms);
-
-    if (!errors) {
+  
+    const validationErrors = validateRegister(username, email, password, confirmPassword, acceptTerms);
+    setErrors(validationErrors);
+  
+    if (Object.keys(validationErrors).length === 0) {
       const user = {
           username,
           email,
           password
       };
-
+  
       try {
-        const response = await axios.post('http://localhost:8000/api-user/register', user);
-        console.log(response.data);
+        const response = await axios.post('http://localhost:8000/api-user/register/', user);
+  
+        if (response.status === 201) {
+            toast.success('Successfully registered!');
+            navigate('/login');
+        } else {
+            toast.error('An error occurred during registration.');
+        }
+  
       } catch (err) {
-          // Assuming the API returns a message in case of an error
-          setErrors({...errors, api: err.response.data.message});
-      }
-    } else {
-        // Handle the errors
-        setErrors(errors);
+        if (err.response && err.response.data && err.response.data.email) {
+            setErrors({...errors, email: err.response.data.email[0]});
+        } else if (err.response && err.response.data && err.response.data.message) {
+            setErrors({...errors, api: err.response.data.message});
+        } else {
+            toast.error('An error occurred during registration.');
+        }
+      }      
     }
-};
+  }; 
 
 return (
   <form className="register-form" onSubmit={handleSubmit}>
@@ -46,7 +58,6 @@ return (
     <InputField type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" error={errors.email} />
     <InputField type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" error={errors.password} />
     <InputField type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} placeholder="Confirm Password" error={errors.confirmPassword} />
-  
     <div className="checkbox-container">
       <input type="checkbox" checked={acceptTerms} onChange={e => setAcceptTerms(e.target.checked)} />
       <span>I accept the terms and conditions</span>
