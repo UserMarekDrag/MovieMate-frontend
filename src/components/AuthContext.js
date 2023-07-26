9593989d3b01197a86cc5eea82c7cbec33805d54
Marek <marekdrag@gmail.com>
2023-07-26 15:19:39 +0200

    Added AuthContext to manage user authentication state

---
+++ src/components/AuthContext.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const checkAuth = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/api-user/user/', {
+        headers: { 'Authorization': 'Token ' + localStorage.getItem('token') }
+      });
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error(error);
+      localStorage.removeItem('token');
+      setIsAuthenticated(false);
+    }
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, checkAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
