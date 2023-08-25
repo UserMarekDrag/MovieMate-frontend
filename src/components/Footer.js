af0ef38a1bad9c7f2a7dadbe9eefc4dcaea03429
Marek <marekdrag@gmail.com>
2023-08-25 13:54:04 +0200

    Remove footer-links div from Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,6 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-links">
-          <Link to="/privacy-policy">Privacy</Link>
-          <Link to="/terms-of-service">Terms</Link>
-        </div>
         <p>&copy; {new Date().getFullYear()} MovieMate by Marek Drąg</p>
       </div>
     </footer>
